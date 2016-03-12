0ad11bcfec2eb2ad63f31eea3bcb3fbcfa364931
Juri Stoinov <juri@celtis.net>
2016-03-12 11:14:18 +0200

    you can specify the path and the map size is calculated dynamically

--- game.js
+++ game.js
@@ -8,19 +8,35 @@ var mapsLibrary = [
 		[1,1,1,0],
 		[2,0,1,0],
 		[0,0,0,0]
+	],
+	[
+		[3,0,0,0,0],
+		[1,1,1,0,0],
+		[2,0,1,0,0],
+		[0,0,0,0,0]
 	]
 ]
 
 var main;
+var path;
+
+const boxWidth = 80;
+const boxMargin = 5;
+const boxBorder = 1;
 
 $(function() {
 	main = $("main");
+	path = $("nav.path");
+	$("nav.controls").click(buttonPressed);
 	initLevel(mapsLibrary[0]);
 });
 
 function initLevel(map) {
 	main.empty();
-	main.css({width: "400px"});
+	path.empty();
+	var rowElements = map[0].length;
+	var containerWidth = rowElements * (boxWidth + 2*boxBorder + 2*boxMargin);
+	main.css({width: containerWidth + "px"});
 	for (var i in map) {
 		var row = map[i];
 		for (var j in row) {
@@ -29,11 +45,9 @@ function initLevel(map) {
 	}
 }
 
-function createBlock(type)
-{
+function createBlock(type) {
 	var block = $(document.createElement("div"));
-	switch (type)
-	{
+	switch (type) {
 		case 1:
 			block.addClass("wall");
 			break;
@@ -48,6 +62,51 @@ function createBlock(type)
 	}
 
 	return block;
+}
+
+function buttonPressed(e) {
+	var btn = $(e.target);
+	var action = btn.data("action");
+	var step = createPathStep(action);
+	path.append(step);
+}
+
+function createPathStep(type) {
+	var step = $(document.createElement("i"));
+	step.data("action", type);
+
+	switch (type) {
+		case "left":
+			step.addClass("fa fa-chevron-circle-left");
+			break;
+
+		case "up":
+			step.addClass("fa fa-chevron-circle-up");
+			break;
+
+		case "down":
+			step.addClass("fa fa-chevron-circle-down");
+			break;
+
+		case "right":
+			step.addClass("fa fa-chevron-circle-right");
+			break;
+
+		case "go":
+			step.addClass("fa fa-play-circle");
+			$("nav.controls").off("click");
+			tracePath();
+			break;
+	}
+
+	return step;
+}
+
+function tracePath() {
+	path.children().each(function(idx, step) {
+		var move = $(step).data("action");
+		console.log(move)
+	});
 }
 
 
