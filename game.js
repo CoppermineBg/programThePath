d04a36fc7f3a9089c55c2c4df6a2218c91a1ff9b
Juri Stoinov <juri@celtis.net>
2016-03-12 15:43:37 +0200

    playable with levels

--- game.js
+++ game.js
@@ -4,16 +4,24 @@
 
 var mapsLibrary = [
 	[
+		[3,0,0],
+		[0,0,0],
+		[0,0,2]
+	],
+	[
 		[3,0,0,0],
 		[1,1,1,0],
 		[2,0,1,0],
 		[0,0,0,0]
 	],
 	[
-		[3,0,0,0,0],
-		[1,1,1,0,0],
-		[2,0,1,0,0],
-		[0,0,0,0,0]
+		[3,0,0,0,1,1,1],
+		[1,1,1,0,0,0,1],
+		[1,0,1,1,1,0,1],
+		[1,0,0,0,0,0,1],
+		[1,1,1,0,0,1,1],
+		[2,0,1,1,0,1,0],
+		[1,0,0,0,0,0,0]
 	]
 ]
 
@@ -21,19 +29,48 @@ var main;
 var path;
 var playerPosition = {x: 0, y: 0};
 var currentMap;
+var gameEnded = true;
+var loadedMapIndex = 0;
 
 const boxWidth = 80;
 const boxMargin = 5;
 const boxBorder = 1;
 
+$(document).keypress(function(e) {
+	switch (e.keyCode) {
+		case 37: //left
+			if (loadedMapIndex > 0) {
+				loadedMapIndex--;
+			}
+			console.log(loadedMapIndex);
+			initLevel(mapsLibrary[loadedMapIndex]);
+			break;
+
+		case 39: //right
+			if (loadedMapIndex < mapsLibrary.length - 1) {
+				loadedMapIndex++;
+			}
+			initLevel(mapsLibrary[loadedMapIndex]);
+			break;
+	}
+
+	$("h1").html("Level " + Number(loadedMapIndex + 1)).show();
+	setTimeout(function() {
+		$("h1").hide();
+	}, 2000);
+	$("nav.controls").off("click");
+	$("nav.controls").click(buttonPressed);
+});
+
 $(function() {
 	main = $("main");
 	path = $("nav.path");
+	initLevel(mapsLibrary[loadedMapIndex]);
 	$("nav.controls").click(buttonPressed);
-	initLevel(mapsLibrary[0]);
 });
 
 function initLevel(map) {
+	$("h1").hide();
 	main.empty();
 	path.empty();
 	var rowElements = map[0].length;
@@ -110,58 +147,67 @@ function createPathStep(type) {
 }
 
 function tracePath() {
-	var gameEnded = false;
+	gameEnded = false;
 	path.children().each(function(idx, step) {
-		if (!gameEnded) {
-			var move = $(step).addClass("played").data("action");
-			switch (move) {
-				case "left":
-					playerPosition.x--;
-					break;
+		setTimeout(function() {
+			if (!gameEnded) {
+				var move = $(step).addClass("played").data("action");
+				switch (move) {
+					case "left":
+						playerPosition.x--;
+						break;
 
-				case "up":
-					playerPosition.y--;
-					break;
+					case "up":
+						playerPosition.y--;
+						break;
 
-				case "down":
-					playerPosition.y++;
-					break;
+					case "down":
+						playerPosition.y++;
+						break;
 
-				case "right":
-					playerPosition.x++;
-					break;
-			}
-
-			try {
-				var blockType = currentMap[playerPosition.y][playerPosition.x];
-				if (typeof blockType == "undefined") {
-					gameEnded = endGame(false);
-				} else {
-					switch (blockType) {
-						case 1: //wall
-							gameEnded = endGame(false);
-							break;
-
-						case 2: //exit
-							gameEnded = endGame(true);
-							break;
-					}
+					case "right":
+						playerPosition.x++;
+						break;
 				}
-			} catch (e) {
-				gameEnded = endGame(false);
+
+				try {
+					var blockType = currentMap[playerPosition.y][playerPosition.x];
+					if (typeof blockType == "undefined") {
+						gameEnded = endGame(false);
+					} else {
+						switch (blockType) {
+							case 1: //wall
+								gameEnded = endGame(false);
+								break;
+
+							case 2: //exit
+								gameEnded = endGame(true);
+							default:
+								$(".player", main).removeClass("player");
+								var idx = currentMap[0].length * playerPosition.y + playerPosition.x;
+								$($("div", main).get(idx)).addClass("player");
+								break;
+						}
+					}
+				} catch (e) {
+					console.log(e);
+					gameEnded = endGame(false);
+				}
 			}
-		}
+		}, idx * 500);
 	});
 }
 
 function endGame(type)
 {
+	var lbl = $("h1");
 	if (type) {
-		alert("You Win!");
+		lbl.html("You win!");
 	} else {
-		alert("You loose!");
+		lbl.html("You lose!");
 	}
 
+	lbl.show();
 	return true;
 }
 
